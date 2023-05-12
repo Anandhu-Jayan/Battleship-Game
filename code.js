2cfbe6fa04565e5294db32ee3ec211617153b8b5
Anandhu-Jayan <anandhujayan369@gmail.com>
2023-05-12 11:44:40 +0530

    Added rules and winning or losing pop up

--- code.js
+++ code.js
@@ -6,21 +6,55 @@ for(let i=0;i<16;i++){
     parentElement.appendChild(childcell);
 }
 
-let chooseItem=["ship","water"];
+let chooseItem=["ship","submarine","water","water","water","water","water","water","water","water","water","water","water","water","water","water"];
+let rollcount=16;
 parentElement.addEventListener("click",(event)=>{
-    
+let randomroll=-1;
     if(event.target.classList.contains("cells") && event.target.style.backgroundImage==""){
-        
-        if(chooseItem[Math.floor(Math.random() * 2) ]==="ship")
+        randomroll=Math.floor(Math.random() * rollcount);
+        rollcount--;
+        if(chooseItem[randomroll]==="ship"){
             event.target.style.cssText="background-image:url('https://i.ibb.co/ckCh4BW/battleship-ship-boat-army-military-svgrepo-com.jpg')";
-        else
+            chooseItem.splice(randomroll,1);
+            popalert("Won")
+            
+        }
+        else if(chooseItem[randomroll]==="submarine"){
+            event.target.style.cssText="background-image:url('https://i.ibb.co/HB5BSwG/submarine-underwater-sea-ship-war-svgrepo-com.jpg')";
+            chooseItem.splice(randomroll,1);
+            popalert("Lost");
+            
+        }
+        else{
             event.target.style.cssText="background-image:url('https://i.ibb.co/xjPrzfb/waves.jpg');";
+            chooseItem.splice(randomroll,1);
+        }
     }
 })
-let rButton=document.querySelector("button");
+let rButton=document.querySelector("#reset");
 rButton.onclick=()=>{
     let cells=document.querySelectorAll(".cells");
     for(let cell of cells){
         cell.style.backgroundImage="";
+        chooseItem=["ship","submarine","water","water","water","water","water","water","water","water","water","water","water","water","water","water"];
+        rollcount=16;
+    } 
+}
+const popalert=(result)=>{
+    const pop=document.querySelector(".popup");
+    const resultElement=document.querySelector("#result");
+    pop.style.display="block";
+    resultElement.textContent=`You ${result}`;
+
+}
+let paButton=document.querySelector("#playagain");
+paButton.onclick=()=>{
+    const pop=document.querySelector(".popup");
+    pop.style.display="none";
+    let cells=document.querySelectorAll(".cells");
+    for(let cell of cells){
+        cell.style.backgroundImage="";
+        chooseItem=["ship","submarine","water","water","water","water","water","water","water","water","water","water","water","water","water","water"];
+        rollcount=16;
     } 
 }
